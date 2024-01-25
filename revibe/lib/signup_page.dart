3e6074ee5ce7366b9835ee7cc52af4e8ce133c82
Tisha Jhabak <iamtisha22@gmail.com>
2024-01-25 17:01:32 +1100

    db integrated

--- revibe/lib/signup_page.dart
+++ revibe/lib/signup_page.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'main.dart'; // Import the SignUpPage widget
+import 'dart:async'; 
 
 class SignUpPage extends StatelessWidget {
   TextEditingController firstNameController = TextEditingController();
@@ -33,7 +36,7 @@ class SignUpPage extends StatelessWidget {
             ),
             SizedBox(height: 16),
             ElevatedButton(
-              onPressed: () {
+              onPressed: () async {
                 // form submission logic here
                 String firstName = firstNameController.text;
                 String emailAddress = emailAddressController.text;
@@ -44,9 +47,9 @@ class SignUpPage extends StatelessWidget {
                 print('First Name: $firstName');
                 print('Email Address: $emailAddress');
 
-
                 // Perform further actions, for example, send data to Firebase
-                _submitToFirebase(firstName, emailAddress, password);
+                await _submitToFirebase(context, firstName, emailAddress, password);
+
               },
               child: const Text('Submit'),
             ),
@@ -57,22 +60,30 @@ class SignUpPage extends StatelessWidget {
   }
 
   // Example function to submit data to Firebase
-  void _submitToFirebase(String firstName, String emailAddress, String password) {
+  Future<void> _submitToFirebase(
+      BuildContext context, String firstName, String emailAddress, String password) async {
     // Your Firebase logic here, e.g., using Firebase Realtime Database or Firestore
     // Note: Ensure you have initialized Firebase in your app before using these services
     // Example:
-    // FirebaseDatabase.instance.reference().child("users").push().set({
-    //   "firstName": firstName,
-    //   "emailAddress": emailAddress,
-    //   "password": password,
-    // });
+    final collection = FirebaseFirestore.instance.collection('signup');
+    try {
+      await collection.doc().set(
+        {
+          'timestamp': FieldValue.serverTimestamp(),
+          'firstName': firstName,
+        },
+      );
+      // ignore: use_build_context_synchronously
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MyApp(),
+        ),
+      );
+    } catch (error) {
+      print('Error submitting to Firebase: $error');
+      // Handle the error appropriately, e.g., show an error message to the user
+    }
 
-    // You can also navigate to another page after successful submission
-    // Navigator.push(
-    //   context,
-    //   MaterialPageRoute(
-    //     builder: (context) => SuccessPage(),
-    //   ),
-    // );
   }
 }
