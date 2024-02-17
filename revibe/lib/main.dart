911439afc486205f45b7033c15ee92899ce4e62f
Kimberlie Fu <kifu5223@uni.sydney.edu.au>
2024-02-17 12:23:01 +0800

    Login/Sign Up

--- revibe/lib/main.dart
+++ revibe/lib/main.dart
@@ -56,11 +56,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-
-
-  
-
-
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -121,7 +116,8 @@ class _MyHomePageState extends State<MyHomePage> {
             children: const [
               Banner(),
               DataBannerV(),
-              InfoBannerV()
+              InfoBannerV(),
+              LoginBannerV()
 
             
             ],
@@ -140,7 +136,8 @@ class _MyHomePageState extends State<MyHomePage> {
               Banner(),
               SizedBox(height: 100.0),
               DataBannerH(),
-              InfoBannerH()
+              InfoBannerH(),
+              LoginBannerH()
             ],
           ),
         )
@@ -182,9 +179,6 @@ class Banner extends StatelessWidget {
   }
 
 }
-
-
-
 
 class DataBannerV extends StatelessWidget {
   const DataBannerV({Key? key});
@@ -277,7 +271,7 @@ class DataBannerH extends StatelessWidget {
               child: Image.asset(
                 'assets/recycle.png',
                 width: MediaQuery.of(context).size.width * 0.4,
-                height: 400, 
+                height: 350, 
               ),
             ),
 
@@ -338,7 +332,7 @@ class InfoBannerV extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: Container(
         height: 800,
-        color: Color.fromARGB(255, 235, 242, 221), 
+        color: const Color.fromARGB(255, 209, 224, 181), 
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -464,13 +458,6 @@ class InfoBannerV extends StatelessWidget {
                 ),
               ],
             ),
-
-          
-
-           
-          
-
-         
           ],
         ),
       ),
@@ -484,10 +471,10 @@ class InfoBannerH extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(15.0),
       child: Container(
         height: 500,
-        color: Color.fromARGB(255, 235, 242, 221), 
+        color: const Color.fromARGB(255, 209, 224, 181), 
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -597,7 +584,6 @@ class InfoBannerH extends StatelessWidget {
 
             const SizedBox(width: 25.0),
 
-
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: Image.asset(
@@ -606,11 +592,6 @@ class InfoBannerH extends StatelessWidget {
                 height: 300, 
               ),
             ),
-
-           
-          
-
-         
           ],
         ),
       ),
@@ -618,8 +599,319 @@ class InfoBannerH extends StatelessWidget {
   }
 }
 
+class LoginBannerV extends StatelessWidget {
+  const LoginBannerV({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        height: 800,
+        color: const Color.fromARGB(255, 209, 224, 181),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                height: double.infinity,
+                color: const Color(0xFFDDF2E8), 
+                child: Column(
+                  children: [
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 75.0, 20.0, 10.0),
+                      child: Text(
+                        'Welcome ReVibers',
+                        style: GoogleFonts.workSans(
+                          textStyle: const TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF107208),
+                            height: 1, 
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 100.0, 20.0, 50.0),
+                      child: Text(
+                        "Let's join forces and make a collective effort to recycle, aiming to create a positive impact on the world starting from today!",
+                        style: GoogleFonts.workSans(
+                          textStyle: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            height: 1, 
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AccountMiddlewarePage(actionType: 1),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered)) {
+                            return const Color(0xFF107208); 
+                          }
+                          return Colors.white;
+                        }),
+                        foregroundColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered)) {
+                            return Colors.white; 
+                          }
+                          return Colors.black;
+                        }),
+                        minimumSize: MaterialStateProperty.all<Size>(const Size(200, 50)),
+                      ),
+                      child: const Text('Log in'),
+                    ),
+
+                  ],
+                )
+              ),
+            ),
+            
+
+            Expanded(
+              child: Container(
+                height: double.infinity,
+                color: const Color(0xFFF0F3EF), 
+                child: Column(
+                  children: [
 
 
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 75.0, 20.0, 10.0),
+                      child: Text(
+                        'Not a Member?',
+                        style: GoogleFonts.workSans(
+                          textStyle: const TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF107208),
+                            height: 1, 
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 100.0, 20.0, 20.0),
+                      child: Text(
+                        "Join our community to save the environment. Embrace sustainability, reduce waste, and advocate for a greener future. Together, let's make a lasting impact for a healthier planet",
+                        style: GoogleFonts.workSans(
+                          textStyle: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            height: 1, 
+                          ),
+                        ),
+                      ),
+                    ),
+
+
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AccountMiddlewarePage(actionType: 2),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered)) {
+                            return const Color(0xFF107208); 
+                          }
+                          return Colors.white;
+                        }),
+                        foregroundColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered)) {
+                            return Colors.white; 
+                          }
+                          return Colors.black;
+                        }),
+                        minimumSize: MaterialStateProperty.all<Size>(const Size(200, 50)),
+                      ),
+                      child: const Text('Sign Up'),
+                    ),
+                  ],
+                )
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginBannerH extends StatelessWidget {
+  const LoginBannerH({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        height: 500,
+        color: const Color.fromARGB(255, 209, 224, 181),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                height: double.infinity,
+                color: const Color(0xFFDDF2E8), 
+                child: Column(
+                  children: [
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 75.0, 20.0, 10.0),
+                      child: Text(
+                        'Welcome ReVibers',
+                        style: GoogleFonts.workSans(
+                          textStyle: const TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF107208),
+                            height: 1, 
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 100.0, 20.0, 50.0),
+                      child: Text(
+                        "Let's join forces and make a collective effort to recycle, aiming to create a positive impact on the world starting from today!",
+                        style: GoogleFonts.workSans(
+                          textStyle: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            height: 1, 
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AccountMiddlewarePage(actionType: 1),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered)) {
+                            return const Color(0xFF107208); 
+                          }
+                          return Colors.white;
+                        }),
+                        foregroundColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered)) {
+                            return Colors.white; 
+                          }
+                          return Colors.black;
+                        }),
+                        minimumSize: MaterialStateProperty.all<Size>(const Size(200, 50)),
+                      ),
+                      child: const Text('Log in'),
+                    ),
+
+                  ],
+                )
+              ),
+            ),
+            
+
+            Expanded(
+              child: Container(
+                height: double.infinity,
+                color: const Color(0xFFF0F3EF), 
+                child: Column(
+                  children: [
+
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 75.0, 20.0, 10.0),
+                      child: Text(
+                        'Not a Member?',
+                        style: GoogleFonts.workSans(
+                          textStyle: const TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF107208),
+                            height: 1, 
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 100.0, 20.0, 20.0),
+                      child: Text(
+                        "Join our community to save the environment. Embrace sustainability, reduce waste, and advocate for a greener future. Together, let's make a lasting impact for a healthier planet",
+                        style: GoogleFonts.workSans(
+                          textStyle: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            height: 1, 
+                          ),
+                        ),
+                      ),
+                    ),
+
+
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AccountMiddlewarePage(actionType: 2),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered)) {
+                            return const Color(0xFF107208); 
+                          }
+                          return Colors.white;
+                        }),
+                        foregroundColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered)) {
+                            return Colors.white; 
+                          }
+                          return Colors.black;
+                        }),
+                        minimumSize: MaterialStateProperty.all<Size>(const Size(200, 50)),
+                      ),
+                      child: const Text('Sign Up'),
+                    ),
+                  ],
+                )
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 
 
